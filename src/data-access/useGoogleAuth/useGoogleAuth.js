d74fe01d208e9f9d56774c46ce50434b159b468d
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-09-04 14:54:28 +0530

    google auth completed

--- src/data-access/useGoogleAuth/useGoogleAuth.js
+++ src/data-access/useGoogleAuth/useGoogleAuth.js
@@ -1,18 +1,25 @@
 import axios from "axios"
 import { useState } from "react"
+import { ACCESS_TOKEN } from "../../constants/constants"
 
 const useGoogleAuth = () => {
   const responseSuccessGoogle = async (response) => {
-    console.log(response)
-    await axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_NODE_BACKEND_URL}/auth/google`,
-      data: { tokenId: response.tokenId },
-    }).then((response) => {
-      console.log(response)
-    })
+    console.log(response.tokenId)
+    try {
+      await axios({
+        method: "POST",
+        url: `${process.env.REACT_APP_NODE_BACKEND_URL}/users/auth/google`,
+        data: { tokenId: response.tokenId },
+      }).then((response) => {
+        localStorage.setItem(ACCESS_TOKEN, response.data.token)
+        console.log(response)
+        // window.location.href = "/dashboard"
+      })
+    } catch (e) {
+      console.log(e)
+    }
 
-    window.location.href = "/dashboard"
+    // window.location.href = "/dashboard"
   }
   const responseErrorGoogle = (response) => {
     console.log(response)
