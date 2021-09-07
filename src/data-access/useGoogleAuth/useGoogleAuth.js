0037a1fca6e8d6f8184d6a46f604750c061ef5e4
Ujjwal Garg <ujjwalgarg.252@gmail.com>
2021-09-07 12:20:52 +0530

    change .env

--- src/data-access/useGoogleAuth/useGoogleAuth.js
+++ src/data-access/useGoogleAuth/useGoogleAuth.js
@@ -1,35 +1,35 @@
-import axios from "axios"
-import { useState } from "react"
-import { COOKIE_NAME } from "../../constants/constants"
-import Cookies from "universal-cookie"
+import axios from "axios";
+import { useState } from "react";
+import { COOKIE_NAME } from "../../constants/constants";
+import Cookies from "universal-cookie";
 
 const useGoogleAuth = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const cookies = new Cookies()
+  const [isLoading, setIsLoading] = useState(false);
+  const cookies = new Cookies();
   const responseSuccessGoogle = async (response) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       await axios({
         method: "POST",
         url: `${process.env.REACT_APP_NODE_BACKEND_URL}/users/auth/google`,
         data: { tokenId: response.tokenId },
       }).then((response) => {
-        cookies.set(COOKIE_NAME, response.data.token)
-        console.log(response)
-        setIsLoading(false)
-        window.location.href = "/dashboard"
-      })
+        cookies.set(COOKIE_NAME, response.data.token);
+        console.log(response);
+        setIsLoading(false);
+        window.location.href = "/dashboard";
+      });
     } catch (e) {
-      console.log(e)
-      setIsLoading(false)
+      console.log(e);
+      setIsLoading(false);
     }
 
     // window.location.href = "/dashboard"
-  }
+  };
   const responseErrorGoogle = (response) => {
-    console.log(response)
-  }
-  return { responseSuccessGoogle, responseErrorGoogle, isLoading, setIsLoading }
-}
+    console.log(response);
+  };
+  return { responseSuccessGoogle, responseErrorGoogle, isLoading, setIsLoading };
+};
 
-export default useGoogleAuth
+export default useGoogleAuth;
